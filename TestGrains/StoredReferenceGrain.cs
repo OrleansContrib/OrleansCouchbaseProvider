67973e64e10b90e31be1597fefcc2823d0b7ceb9
Mario Mifsud <mario.mifsud@kindredplc.com>
2017-03-21 14:39:59 +1100

    Couchbase.NetClient nuget package update to latest stable (required to resolve System.Net.Http assembly loader exceptions due to version conflicts); JSON serialization fix to use Orleans default serialization settings, enabling serialization of grain references.

---
+++ TestGrains/StoredReferenceGrain.cs
@@ -0,0 +1,64 @@
+﻿namespace TestGrains
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Orleans;
+
+    /// <summary>
+    /// Contract for a referenced grain.
+    /// </summary>
+    public interface IStoredReferenceGrain : IGrainWithGuidKey
+    {
+        /// <summary>
+        /// Adds reference tag and timestamp, and saves grain state.
+        /// </summary>
+        /// <param name="referenceTag"></param>
+        /// <returns></returns>
+        Task ReferenceMe(string referenceTag);
+
+        Task<string> GetReferenceTag();
+
+        Task<DateTime> GetReferencedAt();
+    }
+
+    public class ReferenceStorageData
+    {
+        public string ReferenceTag { get; set; }
+
+        public DateTime ReferencedAt { get; set; }
+    }
+
+    /// <inheritdoc />
+    public class StoredReferenceGrain : Grain<ReferenceStorageData>, IStoredReferenceGrain
+    {
+        public override Task OnActivateAsync()
+        {
+            if (State == null)
+            {
+                State = new ReferenceStorageData();
+
+            }
+
+            return base.OnActivateAsync();
+        }
+
+        /// <inheritdoc />
+        public async Task ReferenceMe(string referenceTag)
+        {
+            State.ReferenceTag = referenceTag;
+            State.ReferencedAt = DateTime.UtcNow;
+            await WriteStateAsync();
+        }
+
+        public Task<string> GetReferenceTag()
+        {
+            return Task.FromResult(State.ReferenceTag);
+        }
+
+        public Task<DateTime> GetReferencedAt()
+        {
+            return Task.FromResult(State.ReferencedAt);
+        }
+    }
+}
