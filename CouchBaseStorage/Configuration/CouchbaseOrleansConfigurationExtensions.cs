9c04c377b0813479ffb1eb69a40ea64786ca9a59
mrd1234 <37852649+mrd1234@users.noreply.github.com>
2018-04-06 08:32:17 +1000

    Added custom aggregate exception to replace generic version.

--- CouchBaseStorage/Configuration/CouchbaseOrleansConfigurationExtensions.cs
+++ CouchBaseStorage/Configuration/CouchbaseOrleansConfigurationExtensions.cs
@@ -21,8 +21,8 @@ namespace CouchBaseProviders.Configuration
             var grainExpiriesConfig = (CouchbaseOrleansConfigurationSection)ConfigurationManager.GetSection(couchbaseOrleansConfigurationSectionPath);
             if (grainExpiriesConfig == null || grainExpiriesConfig.GrainExpiries.Count == 0) return result;
 
-            var exceptions = new List<InvalidExpiryValueException>();
-
+            var invalidTimestampExceptions = new List<InvalidExpiryValueException>();
+            
             foreach (GrainExpiry documentExpiry in grainExpiriesConfig.GrainExpiries)
             {
                 if (string.IsNullOrWhiteSpace(documentExpiry.GrainType) || result.ContainsKey(documentExpiry.GrainType)) continue;
@@ -30,28 +30,35 @@ namespace CouchBaseProviders.Configuration
                 TimeSpan expiry;
                 if (!TimeSpan.TryParse(documentExpiry.Expiry, out expiry))
                 {
-                    exceptions.Add(new InvalidExpiryValueException($"Expiry value of '{documentExpiry.Expiry}' for grain type '{documentExpiry.GrainType}' is not in the correct format."));
+                    invalidTimestampExceptions.Add(new InvalidExpiryValueException($"Expiry value of '{documentExpiry.Expiry}' for grain type '{documentExpiry.GrainType}' is not in the correct format."));
                     continue;
                 }
 
                 result.Add(documentExpiry.GrainType, expiry);
             }
 
-            if (exceptions.Any())
+            if (invalidTimestampExceptions.Any())
             {
-                var message = new StringBuilder();
-                message.AppendLine($"{exceptions.Count} document expiry values are invalid. Please see inner exceptions for details then correct your config file.");
-                message.AppendLine();
-                message.AppendLine("Valid examples include:");
-                message.AppendLine($"10 seconds: {TimeSpan.FromSeconds(10)}");
-                message.AppendLine($"10 minutes: {TimeSpan.FromMinutes(10)}");
-                message.AppendLine($"10 hours: {TimeSpan.FromHours(10)}");
-                message.AppendLine($"10 days: {TimeSpan.FromDays(10)}");
-
-                throw new AggregateException(message.ToString(), exceptions);
+                throw GenerateException(invalidTimestampExceptions);
             }
 
             return result;
+        }
+
+        private static Exception GenerateException(IReadOnlyCollection<Exception> exceptions)
+        {
+            var onlyOne = exceptions.Count == 1;
+
+            var message = new StringBuilder();
+            message.AppendLine($"{exceptions.Count} document expiry value{(onlyOne ? " is" : "s are")} invalid. Please see inner exceptions for details then correct your config file.");
+            message.AppendLine();
+            message.AppendLine("Valid examples include:");
+            message.AppendLine($"10 seconds: {TimeSpan.FromSeconds(10)}");
+            message.AppendLine($"10 minutes: {TimeSpan.FromMinutes(10)}");
+            message.AppendLine($"10 hours: {TimeSpan.FromHours(10)}");
+            message.AppendLine($"10 days: {TimeSpan.FromDays(10).ToString().Replace(".", ":")}");
+            
+            return new InvalidDocumentExpiryConfigurationException(message.ToString(), exceptions);
         }
     }
 }
